24817ac9bd61ac1aafc5c15ba5408c7daa24ee64
MrRoss256 <paul.ross@outlook.com>
2023-10-01 20:54:24 +0100

    Upload

---
+++ fpg.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+
+	"os"
+	"time"
+
+	"github.com/pymaxion/geographiclib-go/geodesic"
+	"github.com/pymaxion/geographiclib-go/geodesic/capabilities"
+
+	fpg_types "fpg_types"
+
+	el "elastic"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Given a flight with a departure and arrival time, if the
+// flight is active at the time give.  Calculate how far we have
+// travelled since the departure time (based on pre-canned speed)
+// and find the point for that distance.
+func weThereYet(flt *fpg_types.Flight, dt *time.Time) *fpg_types.FlightLoc {
+
+	var dep = time.Time(flt.Dep.Time)
+	var arr = time.Time(flt.Arr.Time)
+	var now, _ = time.Parse("15:04:05", dt.Format("15:04:05"))
+
+	if now.Before(dep) || now.After(arr) {
+		return nil
+	}
+
+	log.Infof("At [%s] %s is in the air\n", dt.String(), flt.FlightDes)
+
+	// How long has the flight been in the air?
+	var dur = now.Sub(dep)
+
+	// How far have we travelled in that time?
+	var dist = int64(dur.Seconds()) * flt.Speed
+	l := geodesic.WGS84.InverseLine(flt.Dep.Long, flt.Dep.Lat, flt.Arr.Long, flt.Arr.Lat)
+	r := l.PositionWithCapabilities(float64(dist), capabilities.Standard|capabilities.LongUnroll)
+
+	var loc fpg_types.FlightLoc
+	loc.FlightDes = flt.FlightDes
+	t := dt
+	loc.DepTime = time.Date(t.Year(), t.Month(), t.Day(), dep.Hour(), dep.Minute(), dep.Second(), 0, t.Location())
+	loc.Location.Lat = r.Lat2
+	loc.Location.Lon = r.Lon2
+	loc.EventTime = *dt
+	return &loc
+}
+
+func main() {
+	log.SetLevel(log.InfoLevel)
+
+	useElastic := flag.Bool("elastic", false, "Output results to Elasticsearch (localhost)")
+	useConsole := flag.Bool("console", false, "Output results to the console")
+	perpetual := flag.Bool("perpetual", false, "Real time")
+	flag.Parse()
+
+	if *useElastic {
+		el.Connect()
+	}
+
+	// Populate the flts with the json data, should be coming from the TDG
+	// view of active flights
+	var flts fpg_types.Flights
+	jsonFile, err := os.Open("flights.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := io.ReadAll(jsonFile)
+	err = json.Unmarshal(byteValue, &flts)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Calculate some useful values for the flights, flight duration and speed
+	log.Info("Parsed and enriched flights:")
+	for i := range flts.Flights {
+		rag := geodesic.WGS84.Inverse(flts.Flights[i].Dep.Long, flts.Flights[i].Dep.Lat, flts.Flights[i].Arr.Long, flts.Flights[i].Arr.Lat)
+		flts.Flights[i].Duration = int64(time.Time(flts.Flights[i].Arr.Time).Sub(time.Time(flts.Flights[i].Dep.Time)).Seconds())
+		flts.Flights[i].Speed = int64(int64(rag.S12) / flts.Flights[i].Duration)
+		log.Infof("\t%#v\n", flts.Flights[i])
+	}
+
+	if *perpetual {
+		for {
+			t := time.Now()
+			for _, element := range flts.Flights {
+				loc := weThereYet(&element, &t)
+				if loc != nil {
+					if *useConsole == true {
+						var b, _ = json.Marshal(loc)
+						fmt.Println(string(b))
+					}
+					if *useElastic == true {
+						el.Write(loc)
+					}
+				}
+			}
+			time.Sleep(10 * time.Second)
+		}
+	} else {
+		//
+		end := time.Now()
+		start := end.Add(-(24 * time.Hour))
+		for d := start; d.After(end) == false; d = d.Add(1 * time.Minute) {
+			for _, element := range flts.Flights {
+				loc := weThereYet(&element, &d)
+				if loc != nil {
+					if *useConsole == true {
+						var b, _ = json.Marshal(loc)
+						fmt.Println(string(b))
+					}
+					if *useElastic == true {
+						el.Write(loc)
+					}
+				}
+			}
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module fpg
+
+go 1.21.1
+
+require github.com/pymaxion/geographiclib-go v1.0.0
+
+require (
+	github.com/elastic/elastic-transport-go/v8 v8.0.0-20230329154755-1a3c63de0db6 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/elastic/go-elasticsearch/v8 v8.10.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
